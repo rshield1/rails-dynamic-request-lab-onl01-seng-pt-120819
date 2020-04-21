0953774d548dad9ffc5709132bc5e3e68dfd9169
Robert Shields <robert.shieldsiv@gmail.com>
2020-04-21 03:25:39 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,14 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
